ac039866cb25b8db23bb27004e54fd205f401c32
vadniks <whatever@fake.some>
2024-06-17 12:34:50 +0300

    Learning

--- main.cob
+++ main.cob
@@ -1,35 +1,19 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. Main.
-      * Aka calculator
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 Num1 PIC 9 VALUE ZERO.
-       01 Num2 PIC 9 VALUE ZERO.
-       01 Result PIC 99.9 VALUE ZEROS.
-       01 Operator PIC X VALUE "-".
-           88 ValidOperator VALUES "+", "-", "*", "/".
-       
+       01 RepCount PIC 9(4) VALUE ZEROS.
+       01 PrnRepCount PIC Z,ZZ9 VALUE ZEROS.
+       01 NumberOfTimes PIC 9(4) VALUE 1000.
+
        PROCEDURE DIVISION.
        Begin.
-           DISPLAY "Enter a single digit number: " WITH NO ADVANCING
-           ACCEPT Num1
-           DISPLAY "Enter a single digit number: " WITH NO ADVANCING
-           ACCEPT Num2
-           DISPLAY "Enter the operator to be applied: " WITH NO
-               ADVANCING
-           ACCEPT Operator
-
-           EVALUATE Operator
-               WHEN "+" ADD Num2 TO Num1 GIVING Result ROUNDED
-               WHEN "-" SUBTRACT Num2 FROM Num1 GIVING Result ROUNDED
-               WHEN "*" MULTIPLY Num1 BY Num2 GIVING Result ROUNDED
-               WHEN "/" DIVIDE Num1 BY Num2 GIVING Result ROUNDED
-               WHEN OTHER DISPLAY "Invalid operator entered"
-           END-EVALUATE
-
-           IF ValidOperator
-               DISPLAY "Result is: " Result
-           END-IF
-
+           PERFORM VARYING RepCount FROM 0 BY 50 UNTIL 
+                   RepCount = NumberOfTimes
+               MOVE RepCount TO PrnRepCount
+               DISPLAY "Counting " PrnRepCOunt
+           END-PERFORM
+           MOVE RepCount TO PrnRepCount
+           DISPLAY "RepCount " PrnRepCOunt
            STOP RUN.
